468bce4955fb71c0783bbc5e8021ea0084857c5a
tlalexandre <>
2023-10-09 09:18:56 +0000

    Adding Categories

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -3,7 +3,6 @@ from taskmanager import db
 
 class Category(db.Model):
     # schema for the Category model
-
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category",
@@ -15,7 +14,7 @@ class Category(db.Model):
 
 
 class Task(db.Model):
-    # schema for the Category model
+    # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
@@ -26,6 +25,6 @@ class Task(db.Model):
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return "{0} - Task: {1} | Urgent: {2}".format(
+        return "#{0} - Task: {1} | Urgent: {2}".format(
             self.id, self.task_name, self.is_urgent
         )
---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+});
+

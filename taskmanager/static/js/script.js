63c941e7eff4cb352ad1733de20513fc660fdc6e
tlalexandre <>
2023-10-10 09:04:22 +0000

    Setting up for deployement

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,6 +1,23 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    var sidenav = document.querySelectorAll('.sidenav');
+    // sidenav initialization
+    let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+
+        format: "dd mmmm, yyyy",
+        i18n: { done: "Select" }
+    }
+    );
+
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+    // collapsible initialization
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
 });
 

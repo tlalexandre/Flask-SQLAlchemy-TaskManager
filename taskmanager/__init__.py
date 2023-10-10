5830708c4675d3c137cee3259beff7fa240e2139
tlalexandre <>
2023-10-10 09:17:38 +0000

    Setting up for deployement

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -7,13 +7,11 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
- if os.environ.get("DEVELOPMENT") == "True":
-     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
- else:
-     uri = os.environ.get("DATABASE_URL")
-     if uri.startswith("postgres://"):
-         uri = uri.replace("postgres://", "postgresql://", 1)
-     app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
+ uri = os.environ.get("DATABASE_URL")
+ if uri.startswith("postgres://"):
+     uri = uri.replace("postgres://", "postgresql://", 1)
+ app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 
